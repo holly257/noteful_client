4842c41bf272d29325c87192795ec3593bf844f3
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-02-20 07:58:23 -0500

    added header css

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Header.css'
 
 function Header(){
     return(
         <header>
-            <h1>Noteful</h1>
+            <h1 className='header'>Noteful</h1>
             {/* click to nav to main page */}
         </header>
     )
